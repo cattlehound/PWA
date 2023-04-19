61eabc862d7b5f53f5f3ee183aa96025a5f50f42
cattlehound <cattlehound@yahoo.com>
2023-04-19 12:55:02 +1200

    Update service-workers.js

--- static/service-workers.js
+++ static/service-workers.js
@@ -11,7 +11,67 @@ self.addEventListener('install', function(event) {
   });
   
   self.addEventListener('fetch', function(event) {
+    event.respondWith(self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('flask-pwa-cache-v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/static/manifest.json',
+          // Add any other static assets you want to cache
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  });
+  
+  self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    const { title, message } = data;
+  
+    const options = {
+      body: message,
+      icon: 'images/icon.png', // Add an icon image if you want
+    };
+  
+    event.waitUntil(self.registration.showNotification(title, options));
+  
+    // Send a message to the page with the received data
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage(data);
+      });
+    });
+  });
+  
+  self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        }).then(function (clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if (client.url === '/' && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
+  
       caches.match(event.request).then(function(response) {
         if (response) {
           return response;
